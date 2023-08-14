61a57b30ddf0a1077c1dc37b4067a0955b8832d9
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-08-14 17:19:22 -0400

    feat: add yarn scripts

--- packages/celestia-app/src/App.tsx
+++ packages/celestia-app/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Celestia
-    </h1>
-  );  
+    <h1 className="text-3xl font-bold underline text-red-600">Celestia</h1>
+  );
 }
 
 export default App;
