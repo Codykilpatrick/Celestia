f85a7bace0c9209a93c8104e33fa8c08c0fcbf0f
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-08-16 15:05:30 -0400

    feat: add comments migration

---
+++ packages/celestia-database/migrations/sqls/20230816181659-create-item-table-comments-up.sql
@@ -0,0 +1,3 @@
+COMMENT ON TABLE celestia_public.item IS 'An item in the world of Eve Online.';
+COMMENT ON COLUMN celestia_public.item.item_name IS 'The name of an item in the world of Eve Online.';
+COMMENT ON COLUMN celestia_public.item.type_id IS 'An item`s unique id in the world of Eve Online.';
