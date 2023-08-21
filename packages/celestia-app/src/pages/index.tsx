ff9afbae9198f824b4429efabd07875218575d49
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-08-21 16:56:36 -0400

    feat: update page title

--- packages/celestia-app/src/pages/index.tsx
+++ packages/celestia-app/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 className='text-mauve-1'>Welcome to My Next.js App</h1>
+      <h1 className='text-mauve-1'>Celestia</h1>
     </div>
   );
 };
