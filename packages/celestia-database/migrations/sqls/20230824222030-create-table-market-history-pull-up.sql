9b528c93d19cf8428313848dd9ee1073fadd5e94
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-08-24 18:51:16 -0400

    refactor: update history pull table

--- packages/celestia-database/migrations/sqls/20230824222030-create-table-market-history-pull-up.sql
+++ packages/celestia-database/migrations/sqls/20230824222030-create-table-market-history-pull-up.sql
@@ -6,6 +6,7 @@ CREATE TABLE celestia_public.market_history_pull (
   average NUMERIC(12, 2) NOT NULL,
   order_count int,
   region_id int,
+  type_id int,
   volume bigint
 );
 
@@ -16,4 +17,5 @@ COMMENT ON COLUMN celestia_public.market_history_pull.lowest IS 'The lowest an i
 COMMENT ON COLUMN celestia_public.market_history_pull.average IS 'The average price an item sold for on this date in this region.';
 COMMENT ON COLUMN celestia_public.market_history_pull.order_count IS 'The ammount of individual orders for this item in a specific region.';
 COMMENT ON COLUMN celestia_public.market_history_pull.volume IS 'The ammount of individual items across all orders for this item in a specific region.';
-COMMENT ON COLUMN celestia_public.market_history_pull.region_id IS 'The region id that for this data entry.';
+COMMENT ON COLUMN celestia_public.market_history_pull.region_id IS 'The region id that is for this data entry.';
+COMMENT ON COLUMN celestia_public.market_history_pull.type_id IS 'The type id for the item that is for this data entry.'
