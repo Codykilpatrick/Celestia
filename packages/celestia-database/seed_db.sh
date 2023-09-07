669d4146c6800d915c2c669a71a9a04657acb88d
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-09-07 07:21:50 -0400

    feat: update seed script to include locations

--- packages/celestia-database/seed_db.sh
+++ packages/celestia-database/seed_db.sh
@@ -8,9 +8,17 @@ DB_PORT="5432"
 DB_NAME="celestia"
 
 # Define the path to your CSV file
-CSV_FILE="./seed_data/item_ids.csv"
+ITEM_TABLE="./seed_data/item_ids.csv"
 
 # Run the PostgreSQL command to seed the database
 psql "postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" <<EOF
-\COPY celestia_public.item (type_id, item_name) FROM '${CSV_FILE}' DELIMITER ',' CSV HEADER;
+\COPY celestia_public.item (type_id, item_name) FROM '${ITEM_TABLE}' DELIMITER ',' CSV HEADER;
+EOF
+
+# Define the relative path to your second CSV file
+LOCATION_TABLE="./seed_data/location_ids.csv"
+
+# Run the PostgreSQL command to seed the database with the second CSV file
+psql "postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" <<EOF
+\COPY celestia_public.location (region_id, system_id, system_name, region_name) FROM '${LOCATION_TABLE}' DELIMITER ',' CSV HEADER;
 EOF
