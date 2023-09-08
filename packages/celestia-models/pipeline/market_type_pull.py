289b61705da77cceab8c439cd3aee58df333363b
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-09-08 06:48:35 -0400

    feat: update to be async

--- packages/celestia-models/pipeline/market_type_pull.py
+++ packages/celestia-models/pipeline/market_type_pull.py
@@ -1,34 +1,74 @@
-import requests
+import asyncio
+import aiohttp
+import asyncpg
 
-# The URL to pull current market item types.
-url = "https://esi.evetech.net/latest/markets/10000043/types/?datasource=tranquility"
 
-try:
-    # Send a GET request to the URL
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        region_types = response.json()
-
-        #! Print the results
-        # print(region_types)
-    else:
-        print(f"Request failed with status code {response.status_code}")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-
-for type in region_types:
-    history_url = f"https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id={type}"
+async def fetch_history(session, type_id):
+    history_url = f"https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id={type_id}"
     try:
-        response = requests.get(history_url)
+        async with session.get(history_url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return type_id, data
+            else:
+                print(f"Request failed with status code {response.status} for type_id {type_id}")
+                return type_id, None  # Return None in case of an error
+    except Exception as e:
+        print(f"An error occurred for type_id {type_id}: {str(e)}")
+        return type_id, None  # Return None in case of an error
 
-        if response.status_code == 200:
-            list = response.json()
 
-            print(list)
-        else:
-            print(f"Request failed with status code {response.status_code}")
+async def main():
+    # The URL to pull current market item types.
+    url = "https://esi.evetech.net/latest/markets/10000043/types/?datasource=tranquility"
+    database_url = "postgresql://postgres:password@localhost/celestia"
+    conn = await asyncpg.connect(database_url)
+    region_id = 10000043
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    region_types = await response.json()
+                else:
+                    print(f"Request failed with status code {response.status}")
+                    return
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+        return
+
+    tasks = []
+
+    async with aiohttp.ClientSession() as session:  # Create a new session
+        for type_id in region_types:
+            task = asyncio.ensure_future(fetch_history(session, type_id))
+            tasks.append(task)
+
+        # Use an empty dictionary to store results
+        results = {}
+
+        for future in asyncio.as_completed(tasks):
+            type_id, data = await future
+            results[type_id] = data
+            print(f"Fetched data for type_id {type_id}")
+            print(data)
+            if data is not None:
+                for row in data:
+                    await conn.execute(
+                        "INSERT INTO celestia_public.market_history_pull (type_id, date, average, highest, lowest, order_count, volume, region_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+                        type_id,
+                        row['date'],
+                        row['average'],
+                        row['highest'],
+                        row['lowest'],
+                        row['order_count'],
+                        row['volume'],
+                        region_id
+                    )
+
+
+    # Close the database connection
+    await conn.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
