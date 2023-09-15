61782b5a1588e0b57e5bce6c1ce935ee98149a75
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-09-15 10:01:10 -0400

    feat: append the changes to the current db

--- packages/celestia-models/pipeline/sync_market_history.py
+++ packages/celestia-models/pipeline/sync_market_history.py
@@ -7,8 +7,30 @@ def sync_table(connection, region_id):
     query = f"SELECT * FROM celestia_public.market_history_sync WHERE region_id = {region_id};"
     cursor.execute(query)
     new_results = cursor.fetchall()
-    data = pd.DataFrame(new_results, columns=['id', 'date', 'highest', 'lowest', 'average', 'order_count', 'region_id', 'type_id', 'volume'])
-    print(data)
+    new_data = pd.DataFrame(new_results, columns=['id', 'date', 'highest', 'lowest', 'average', 'order_count', 'region_id', 'type_id', 'volume'])
+    query_dates = f"SELECT * FROM celestia_public.market_history_pull WHERE region_id = {region_id};"
+    cursor.execute(query_dates)
+    dates_in_current = cursor.fetchall()
+    dates = pd.DataFrame(dates_in_current, columns=['id', 'date', 'highest', 'lowest', 'average', 'order_count', 'region_id', 'type_id', 'volume'])
+    unique_dates = dates['date'].unique()
+    new_data_trimmed = new_data[~new_data['date'].isin(unique_dates)]
+    
+    # Append new_data_trimmed to the PostgreSQL table
+    insert_query = """
+        INSERT INTO celestia_public.market_history_pull
+        (date, highest, lowest, average, order_count, region_id, type_id, volume)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    """
+    values = [tuple(row) for row in new_data_trimmed[['date', 'highest', 'lowest', 'average', 'order_count', 'region_id', 'type_id', 'volume']].values]
+    
+    try:
+        cursor.executemany(insert_query, values)
+        connection.commit()
+        print(f"{len(new_data_trimmed)} rows appended to market_history_pull for region_id {region_id}")
+    except psycopg2.Error as e:
+        connection.rollback()
+        print(f"Error inserting data: {e}")
+
 
 
 def main():
