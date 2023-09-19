89eb28f22fc81ce547da719f9cae496776922761
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-09-19 08:50:22 -0400

    feat: add footer component

---
+++ packages/celestia-app/src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-violet-dark-3 h-16 m-12 rounded-lg">
+      Footer
+    </div>
+    )
+}
+
+export default Footer;
--- packages/celestia-app/src/pages/index.tsx
+++ packages/celestia-app/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '@/components/header';
 import Hero from '@/components/hero';
+import Footer from '@/components/footer';
 
 const Home = () => {
   return (
     <div className="w-full">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
