b05bb76cc9a264a1afea354a84014773d624a079
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-09-23 07:14:09 -0400

    feat: first query

--- packages/celestia-app/src/pages/index.tsx
+++ packages/celestia-app/src/pages/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+const { ApolloClient, InMemoryCache, gql } = require('@apollo/client');
 
-const Home = () => {
+
+const Home = ({allItems}) => {
+  console.log('all Items', allItems)
   return (
-    <div>
-      <h1 className='text-mauve-1'>Celestia</h1>
+    <div className="w-full">
+      Celestia
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'http://localhost:8080/graphql',
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query allItems {
+        allItems {
+          edges {
+            node {
+              id
+              itemName
+            }
+          }
+        }
+      }
+    `
+  });
+
+  return {
+    props: {
+      allItems: [data]
+    }
+  }
+}
 
 export default Home;
