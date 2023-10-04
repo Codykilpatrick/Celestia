b22d472f4009d4832869383f3d5ea757e4ed9267
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-10-04 07:14:14 -0400

    feat: create link to docs page

--- packages/celestia-app/src/components/header.tsx
+++ packages/celestia-app/src/components/header.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <>
       <div className="bg-violet-4 h-16 mx-12 rounded-b-lg flex-col flex justify-center">
         <ul className="flex justify-between mx-4">
           <li className="text-mauve-12 px-2">Home</li>
-          <li className="text-mauve-12 px-2">Documentation</li>
+          <li className="text-mauve-12 px-2">
+            <Link href="/documentation">Documentation</Link>
+          </li>
         </ul>
       </div>
       <h1 className="text-mauve-12 px-2 mx-12 flex justify-center">Celestia</h1>
