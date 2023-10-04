62c018426ae3b084b21b8f08d5ab99c19fbbb5b8
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-10-04 07:17:27 -0400

    feat: create nav link to home page

--- packages/celestia-app/src/components/header.tsx
+++ packages/celestia-app/src/components/header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
     <>
       <div className="bg-violet-4 h-16 mx-12 rounded-b-lg flex-col flex justify-center">
         <ul className="flex justify-between mx-4">
-          <li className="text-mauve-12 px-2">Home</li>
+          <li className="text-mauve-12 px-2">
+            <Link href="/">Home</Link>
+          </li>
           <li className="text-mauve-12 px-2">
             <Link href="/documentation">Documentation</Link>
           </li>
