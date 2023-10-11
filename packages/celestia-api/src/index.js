ba9ce8139ea0c37e83059719a462227b71c2518b
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-10-11 09:21:17 -0400

    feat: deploy api with fly io

--- packages/celestia-api/src/index.js
+++ packages/celestia-api/src/index.js
@@ -1,15 +1,14 @@
-require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const postgraphile = require('./postgraphile')
 
-const { PORT } = process.env
+const port = process.env.PORT || 8080;
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(postgraphile)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(port, () => console.log(`Server running on port 8080`))
