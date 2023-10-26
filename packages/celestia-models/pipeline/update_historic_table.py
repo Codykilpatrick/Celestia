5d828669230104b326a0e7d7986ee024492b9aa6
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-10-26 08:37:17 -0400

    refactor: update historic table script

--- packages/celestia-models/pipeline/update_historic_table.py
+++ packages/celestia-models/pipeline/update_historic_table.py
@@ -1,9 +1,9 @@
 import subprocess
 
 # Run update_market_history.py
-update_process = subprocess.Popen(['python', 'update_market_history.py'])
-update_process.wait()  # Wait for update_market_history.py to complete
+update_process = subprocess.Popen(['python', 'pull_daily_market_history.py'])
+update_process.wait()
 
 # Run sync_market_history.py
 sync_process = subprocess.Popen(['python', 'sync_market_history.py'])
-sync_process.wait()  # Wait for sync_market_history.py to complete
+sync_process.wait()
