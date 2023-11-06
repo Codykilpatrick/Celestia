6c46f40ece6eb5b2cd538d0b0d1eeb4b0a06eb8b
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-11-06 07:18:56 -0500

    refactor: remove old daily sync logic

--- packages/celestia-models/pipeline/update_historic_table.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# Run update_market_history.py
-update_process = subprocess.Popen(['python', 'pull_daily_market_history.py'])
-update_process.wait()
-
-# Run sync_market_history.py
-sync_process = subprocess.Popen(['python', 'sync_market_history.py'])
-sync_process.wait()
