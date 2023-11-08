99b6cd41c0c5a96c4e058f9a5416a1024814e173
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-11-08 08:21:25 -0500

    refactor: remove dev prod scripts

--- packages/celestia-app/src/apollo/apollo-client.ts
+++ packages/celestia-app/src/apollo/apollo-client.ts
@@ -2,6 +2,8 @@ const { ApolloClient, InMemoryCache } = require('@apollo/client');
 
 const uri = process.env.NODE_ENV === 'production' ? 'https://celestia-api.fly.dev/graphql' : 'http://localhost:8080/graphql';
 
+console.log('uri', uri);
+
 const client = new ApolloClient({
   uri,
   cache: new InMemoryCache(),
