2adc62103fc4b39b0058a1c4233f4ae05ce8af86
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-11-08 08:52:27 -0500

    refactor: linting

--- packages/celestia-app/src/apollo/apollo-client.ts
+++ packages/celestia-app/src/apollo/apollo-client.ts
@@ -1,8 +1,7 @@
 const { ApolloClient, InMemoryCache } = require('@apollo/client');
 
-const uri = process.env.NODE_ENV === 'production' ? 'https://celestia-api.fly.dev/graphql' : 'http://localhost:8080/graphql';
-
-console.log('uri', uri);
+const uri =
+  process.env.NODE_ENV === 'production' ? 'https://celestia-api.fly.dev/graphql' : 'http://localhost:8080/graphql';
 
 const client = new ApolloClient({
   uri,
