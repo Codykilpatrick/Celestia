417806652bc5c32eaf9a62e6510f6147d7e2b6c0
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-11-09 07:36:06 -0500

    refactor: head style changes

--- packages/celestia-app/src/components/header.tsx
+++ packages/celestia-app/src/components/header.tsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <div className="bg-black h-16  flex-col flex justify-center">
       <ul className="flex justify-between mx-4">
-        <li className="text-mauve-12 px-2 hover:text-blue-500">
+        <li className="text-mauve-11 px-2 hover:text-mauve-12">
           <Link href="/">Home</Link>
         </li>
-        <li className="text-mauve-12 px-2 hover:text-blue-500">
+        <li className="text-mauve-11 px-2 hover:text-mauve-12">
           <Link href="/blog">Dev Blog</Link>
         </li>
-        <li className="text-mauve-12 px-2 hover:text-blue-500">
+        <li className="text-mauve-11 px-2 hover:text-mauve-12">
           <Link href="/feedback">Feedback</Link>
         </li>
       </ul>
